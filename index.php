9d1792c8db5b57eaa2bbd7a37c9748ffd4e86323
Scognamiglio Loïc <loic.scognamiglio16@gmail.com>
2021-07-06 00:02:55 +0200

    Gestion mobile... Ma vie c'est dla merde

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ if(in_array($page,scandir("action"))){
         <title>Souverain des âmes</title>
         <meta charset="UTF-8">
         <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
         <link href="styles/main.css" rel="stylesheet" type="text/css">
         <script
